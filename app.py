b535d642e00151c1ef572b80de6ea9155c0d809c
Nguyễn Duy Hậu <nguyen.duy.hau@sun-asterisk.com>
2024-01-02 02:28:07 +0700

    Change homepage layout and using API instead

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ import tensorflow_hub as hub
 from io import BytesIO
 from PIL import Image
 from keras.preprocessing import image
-# import mysql.connector
-# from flask_cors import CORS  # Import CORS
 import base64
 import json
 import cv2
@@ -34,48 +32,54 @@ def aboutus():
 
 @app.route('/convert', methods=['POST'])
 def upload_image():
-    file = request.files['image']
+    try:
+        file = request.files['image']
 
-    if file:
-        img = Image.open(BytesIO(file.read()))
+        if file:
+            img = Image.open(BytesIO(file.read()))
 
-        img_array = np.array(img)
-        # img_array = cv2.resize(img_array, (128, 128))
+            img_array = np.array(img)
 
-        # img_array = np.clip(img_array, 0, 1)
-        img_array_normalize = img_array / 255.0
-        print(img_array_normalize.shape)
-        print(np.max(img_array_normalize))
-        # 128x128
+            if img_array.shape != (128, 128):
+                # Resize ảnh về kích thước 128x128
+                img_array = cv2.resize(img_array, (128, 128), interpolation=cv2.INTER_AREA)
+
+            img_array_normalize = img_array / 255.0
+            print(img_array_normalize.shape)
+            print(np.max(img_array_normalize))
+            # 128x128
 
 
-        img_array_1 = np.expand_dims(img_array_normalize, axis=-1)
-        print(img_array_1.shape)
-        #128x128x1
+            img_array_1 = np.expand_dims(img_array_normalize, axis=-1)
+            print(img_array_1.shape)
+            #128x128x1
 
-        image_to_predict = np.expand_dims(img_array_1, axis=0)
-        predictions = model.predict(image_to_predict)
-        reconstructed_image = predictions[0]
-        print("Shape of the reconstructed_image array:",reconstructed_image.shape)
-        ##128x128x1
-        
-        result = np.squeeze(reconstructed_image)
-        print("Shape of the result array:",result.shape)
+            image_to_predict = np.expand_dims(img_array_1, axis=0)
+            predictions = model.predict(image_to_predict)
+            reconstructed_image = predictions[0]
+            print("Shape of the reconstructed_image array:",reconstructed_image.shape)
+            ##128x128x1
+            
+            result = np.squeeze(reconstructed_image)
+            print("Shape of the result array:",result.shape)
 
-        result = (result * 255).astype(np.uint8)
-        ## result shape = 128x128
+            result = (result * 255).astype(np.uint8)
+            ## result shape = 128x128
 
-        # Resize result từ 128x128 thành 512x512
-        result_resized = cv2.resize(result, (512, 512))
+            # Resize result từ 128x128 thành 512x512
+            result_resized = cv2.resize(result, (512, 512))
 
-         # Mã hóa mảng NumPy thành base64
-        _, buffer = cv2.imencode('.png', result_resized)
-        img_base64 = base64.b64encode(buffer).decode('utf-8')
+            # Mã hóa mảng NumPy thành base64
+            _, buffer = cv2.imencode('.png', result_resized)
+            img_base64 = base64.b64encode(buffer).decode('utf-8')
 
-        # return jsonify({
-        #     "img": img_base64
-        # })
-        return render_template('result.html', image_array=img_base64)
+            return jsonify({
+                "img": img_base64
+            })
+            # return render_template('result.html', image_array=img_base64)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 
 
 if __name__ == '__main__':
